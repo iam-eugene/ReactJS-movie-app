8d846208da9d5de924123ae1a0613f3b20fc3076
iam-eugene <mai77eugene@gmail.com>
2022-05-19 01:21:00 +0900

    chapter6 - 6.2 - Deps

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,16 +2,42 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
+  const [keyword, setKeyword] = useState('');
 
-  console.log('i run all the time');
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log('CALL THE API...');
+    console.log('I run only once');
   }, []);
+
+  useEffect(() => {
+    console.log('I run when "keyword" changes');
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log('I run when "counter" changes');
+  }, [counter]);
+
+  useEffect(() => {
+    console.log('I run when keyword & counter changes');
+  }, [keyword, counter]);
+
+  // 조건
+  // useEffect(() => {
+  //   if (keyword !== '' && keyword.length > 5) {
+  //     console.log('SEARCH FOR', keyword);
+  //   }
+  // }, [keyword]);
 
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
